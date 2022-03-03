561e022cad0a6b67e9495f03398dd9529ceca9d1
Netson <netsoncavina@gmail.com>
2022-03-03 20:03:40 -0300

    added team info

--- tinder-frontend/src/components/Card.js
+++ tinder-frontend/src/components/Card.js
@@ -5,22 +5,60 @@ import "./styles/Card.css";
 function Card() {
   const [people, setPeople] = useState([
     {
-      name: "Elon Musk",
-      url: "https://www.biography.com/.image/t_share/MTY2MzU3Nzk2OTM2MjMwNTkx/elon_musk_royal_society.jpg",
+      name: "Drop",
+      url: "https://pbs.twimg.com/media/FGhoEmvXwAA70xp?format=png&name=small",
+      team: "Netshoes Miners",
     },
     {
-      name: "Jeff Bezos",
-      url: "https://media.seudinheiro.com/cdn-cgi/image/fit=contain,width=640&,format=auto/uploads/2021/02/shutterstock_1886492299-628x353.jpg",
+      name: "Tinowns",
+      url: "https://pbs.twimg.com/media/FJuXeHyXMAQnrB-?format=jpg&name=large",
+      team: "LOUD",
+    },
+    {
+      name: "Cariok",
+      url: "https://pbs.twimg.com/profile_images/1471152882070208516/PxzMRpy5_400x400.jpg",
+      team: "Pain",
+    },
+
+    {
+      name: "Ranger",
+      url: "https://pbs.twimg.com/profile_images/1464262213829545990/G8IANV30_400x400.jpg",
+      team: "Furia",
+    },
+    {
+      name: "Boal",
+      url: "https://pbs.twimg.com/media/FCVnAohXEAog7ST?format=png&name=small",
+      team: "Flamengo",
+    },
+    {
+      name: "Revolta",
+      url: "https://pbs.twimg.com/media/EryIEO1XcAQabAK?format=jpg&name=large",
+      team: "INTZ",
+    },
+    {
+      name: "brTT",
+      url: "https://cdn.ome.lt/mk7v0YySwu31SB_VQbGsZPi-2D4=/770x0/smart/uploads/conteudo/fotos/brtt-capa-1024x702.jpg",
+      team: "Pain",
+    },
+    {
+      name: "Micao",
+      url: "https://pbs.twimg.com/profile_images/1456002425651929097/fWHy0lNN_400x400.jpg",
+      team: "INTZ",
+    },
+    {
+      name: "Titan",
+      url: "https://pbs.twimg.com/media/FGxjJmxXsAQI4YW?format=jpg&name=medium",
+      team: "Red Canids",
     },
   ]);
 
   const swiped = (direction, nameToDelete) => {
-    console.log("removing: " + nameToDelete);
+    console.log("removing: " + nameToDelete, direction);
     // setLastDirection(direction);
   };
 
   const outOfFrame = (name) => {
-    console.log(name + " left the screen!");
+    // console.log(name + " left the screen!");
   };
 
   return (
---
+++ tinder-frontend/src/components/data.js
@@ -0,0 +1,39 @@
+const data = [
+  {
+    name: "Drop",
+    url: "https://pbs.twimg.com/media/FGhoEmvXwAA70xp?format=png&name=small",
+  },
+  {
+    name: "Tinowns",
+    url: "https://pbs.twimg.com/media/FJuXeHyXMAQnrB-?format=jpg&name=large",
+  },
+  {
+    name: "Cariok",
+    url: "https://pbs.twimg.com/profile_images/1471152882070208516/PxzMRpy5_400x400.jpg",
+  },
+
+  {
+    name: "Ranger",
+    url: "https://pbs.twimg.com/profile_images/1464262213829545990/G8IANV30_400x400.jpg",
+  },
+  {
+    name: "Boal",
+    url: "https://pbs.twimg.com/media/FCVnAohXEAog7ST?format=png&name=small",
+  },
+  {
+    name: "Revolta",
+    url: "https://pbs.twimg.com/media/EryIEO1XcAQabAK?format=jpg&name=large",
+  },
+  {
+    name: "brTT",
+    url: "https://cdn.ome.lt/mk7v0YySwu31SB_VQbGsZPi-2D4=/770x0/smart/uploads/conteudo/fotos/brtt-capa-1024x702.jpg",
+  },
+  {
+    name: "Micao",
+    url: "https://pbs.twimg.com/profile_images/1456002425651929097/fWHy0lNN_400x400.jpg",
+  },
+  {
+    name: "Titan",
+    url: "https://pbs.twimg.com/media/FGxjJmxXsAQI4YW?format=jpg&name=medium",
+  },
+];
