910635e25f6ce1419fe6671e2422ca6073ebf815
Netson <netsoncavina@gmail.com>
2022-03-06 03:07:38 -0300

    added team data

--- tinder-frontend/src/components/data.js
+++ tinder-frontend/src/components/data.js
@@ -1,39 +1,53 @@
 const data = [
   {
     name: "Drop",
-    url: "https://pbs.twimg.com/media/FGhoEmvXwAA70xp?format=png&name=small",
+    imgUrl: "https://pbs.twimg.com/media/FGhoEmvXwAA70xp?format=png&name=small",
+    team: "Netshoes Miners",
   },
   {
     name: "Tinowns",
-    url: "https://pbs.twimg.com/media/FJuXeHyXMAQnrB-?format=jpg&name=large",
+    imgUrl: "https://pbs.twimg.com/media/FJuXeHyXMAQnrB-?format=jpg&name=large",
+    team: "LOUD",
   },
   {
     name: "Cariok",
-    url: "https://pbs.twimg.com/profile_images/1471152882070208516/PxzMRpy5_400x400.jpg",
+    imgUrl:
+      "https://pbs.twimg.com/profile_images/1471152882070208516/PxzMRpy5_400x400.jpg",
+    team: "Pain",
   },
 
   {
     name: "Ranger",
-    url: "https://pbs.twimg.com/profile_images/1464262213829545990/G8IANV30_400x400.jpg",
+    imgUrl:
+      "https://pbs.twimg.com/profile_images/1464262213829545990/G8IANV30_400x400.jpg",
+    team: "Furia",
   },
   {
     name: "Boal",
-    url: "https://pbs.twimg.com/media/FCVnAohXEAog7ST?format=png&name=small",
+    imgUrl: "https://pbs.twimg.com/media/FCVnAohXEAog7ST?format=png&name=small",
+    team: "Flamengo",
   },
   {
     name: "Revolta",
-    url: "https://pbs.twimg.com/media/EryIEO1XcAQabAK?format=jpg&name=large",
+    imgUrl: "https://pbs.twimg.com/media/EryIEO1XcAQabAK?format=jpg&name=large",
+    team: "INTZ",
   },
   {
     name: "brTT",
-    url: "https://cdn.ome.lt/mk7v0YySwu31SB_VQbGsZPi-2D4=/770x0/smart/uploads/conteudo/fotos/brtt-capa-1024x702.jpg",
+    imgUrl:
+      "https://cdn.ome.lt/mk7v0YySwu31SB_VQbGsZPi-2D4=/770x0/smart/uploads/conteudo/fotos/brtt-capa-1024x702.jpg",
+    team: "Pain",
   },
   {
     name: "Micao",
-    url: "https://pbs.twimg.com/profile_images/1456002425651929097/fWHy0lNN_400x400.jpg",
+    imgUrl:
+      "https://pbs.twimg.com/profile_images/1456002425651929097/fWHy0lNN_400x400.jpg",
+    team: "INTZ",
   },
   {
     name: "Titan",
-    url: "https://pbs.twimg.com/media/FGxjJmxXsAQI4YW?format=jpg&name=medium",
+    imgUrl:
+      "https://pbs.twimg.com/media/FGxjJmxXsAQI4YW?format=jpg&name=medium",
+    team: "Red Canids",
   },
 ];
